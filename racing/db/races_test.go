42cc8e087382957c86b7262efd5c717706db0f6a
sergi <sergi@redeye.co>
2023-05-16 15:56:25 +1000

    feat(filter) Adding another filter to the existing RPC, so we can call ListRaces asking for races that are visible only.

---
+++ racing/db/races_test.go
@@ -0,0 +1,48 @@
+package db
+
+import (
+	"database/sql"
+	"git.neds.sh/matty/entain/racing/proto/racing"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestRacesRepoVisibleFilter_List(t *testing.T) {
+
+	racesRepo := createRepo(t)
+
+	visible := true
+	// Set upa  filter with visibile true
+	filter := &racing.ListRacesRequestFilter{
+		Visible: &visible,
+	}
+	races, err := racesRepo.List(filter)
+	if err != nil {
+		return
+	}
+
+	for _, race := range races {
+		assert.Truef(t, race.Visible, "Race %d is not visible.", race.Id)
+	}
+
+}
+
+func TestRacesRepoAll_List(t *testing.T) {
+	racesRepo := createRepo(t)
+	// Set up a filter to pass to the List method
+	filter := &racing.ListRacesRequestFilter{}
+	races, err := racesRepo.List(filter)
+	assert.NoError(t, err)
+	assert.Equalf(t, 100, len(races), "There should be a total of 100 races in DB.")
+}
+
+func createRepo(t *testing.T) RacesRepo {
+	racingDB, err := sql.Open("sqlite3", ":memory:")
+	assert.NoError(t, err)
+
+	repo := NewRacesRepo(racingDB)
+	err = repo.Init()
+	assert.NoError(t, err)
+
+	return repo
+}
